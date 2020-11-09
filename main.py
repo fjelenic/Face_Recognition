fd35e68493231c1865955f333c8c332d2296a804
fjelenic <70028296+fjelenic@users.noreply.github.com>
2020-11-09 16:36:26 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from PIL import Image
+import cv2
+from keras.models import load_model
+import numpy as np
+
+
+model = load_model('model.h5')
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+def face_extractor(img):
+    faces = face_cascade.detectMultiScale(img, 1.3, 3)
+
+    if faces is ():
+        return None
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cropped_face = img[y:y + h, x:x + w]
+
+    return cropped_face
+
+
+video_capture = cv2.VideoCapture(0)
+while True:
+    _, frame = video_capture.read()
+
+    face = face_extractor(frame)
+    if type(face) is np.ndarray:
+        face = cv2.resize(face, (224, 224))
+        img = Image.fromarray(face, 'RGB')
+        img_array = np.array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        pred = model.predict(img_array)
+        print(pred)
+
+        name = "Not me"
+
+        if pred[0][3] > 0.5:
+            name = 'Me'
+        cv2.putText(frame, name, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+    else:
+        cv2.putText(frame, "No face found", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
---
+++ model.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+from keras.layers import Dense, Flatten
+from keras.models import Model
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import ImageDataGenerator
+from glob import glob
+import matplotlib.pyplot as plt
+
+IMAGE_SIZE = [224, 224]
+
+train_path = 'Datasets/Train'
+valid_path = 'Datasets/Validate'
+
+vgg = VGG16(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+
+for layer in vgg.layers:
+    layer.trainable = False
+
+folders = glob(train_path + '/*')
+
+x = Flatten()(vgg.output)
+prediction = Dense(len(folders), activation='sigmoid')(x)
+
+model = Model(inputs=vgg.input, outputs=prediction)
+
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy']
+)
+
+
+train_data_generator = ImageDataGenerator(rescale=1. / 255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+test_data_generator = ImageDataGenerator(rescale=1. / 255)
+
+training_set = train_data_generator.flow_from_directory(train_path,
+                                                 target_size=(224, 224),
+                                                 batch_size=32,
+                                                 class_mode='binary',
+                                                 shuffle=True)
+
+test_set = test_data_generator.flow_from_directory(valid_path,
+                                            target_size=(224, 224),
+                                            batch_size=32,
+                                            class_mode='binary')
+
+
+r = model.fit_generator(
+    training_set,
+    validation_data=test_set,
+    epochs=5,
+    steps_per_epoch=len(training_set),
+    validation_steps=len(test_set)
+)
+
+# loss
+plt.plot(r.history['loss'], label='train loss')
+plt.plot(r.history['val_loss'], label='val loss')
+plt.legend()
+plt.show()
+plt.savefig('LossVal_loss')
+
+# accuracies
+plt.plot(r.history['acc'], label='train acc')
+plt.plot(r.history['val_acc'], label='val acc')
+plt.legend()
+plt.show()
+plt.savefig('AccVal_acc')
+
+
+model.save('model.h5')
